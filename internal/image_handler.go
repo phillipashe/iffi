7f5afd6fb186d0331133e807f1b92384f1bcc479
phillipashe <ashephillip@gmail.com>
2023-03-12 10:02:00 -0400

    placeholder code for protobuf endpoint

--- internal/image_handler.go
+++ internal/image_handler.go
@@ -1,5 +1,24 @@
 package image_handler
 
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
 func HandleImage() string {
-	return "hello world"
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("failed to listen: %v, err")
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
 }
